5aa544dc4fb317e74b4ccd100b430a3c90b9dc09
Heorhii Shakula <swrqmail@gmail.com>
2019-09-24 11:35:12 +0300

    Fix linkage error against Houdini 18.0.236 USD package

--- pxr/imaging/plugin/hdRpr/rendererPlugin.cpp
+++ pxr/imaging/plugin/hdRpr/rendererPlugin.cpp
@@ -21,7 +21,7 @@
 // KIND, either express or implied. See the Apache License for the specific
 // language governing permissions and limitations under the Apache License.
 //
-#include "pxr/imaging/hdx/rendererPluginRegistry.h"
+#include "pxr/imaging/hd/rendererPluginRegistry.h"
 #include "renderDelegate.h"
 #include "rendererPlugin.h"
 
@@ -30,7 +30,7 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 TF_REGISTRY_FUNCTION(TfType)
 {
-    HdxRendererPluginRegistry::Define<HdRprPlugin>();
+    HdRendererPluginRegistry::Define<HdRprPlugin>();
 }
 
 HdRenderDelegate*
