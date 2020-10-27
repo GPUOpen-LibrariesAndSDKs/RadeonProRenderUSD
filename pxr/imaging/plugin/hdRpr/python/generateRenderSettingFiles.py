874073ae48a165475e4dd07d96c3c53aeb58058f
hshakula <swrqmail@gmail.com>
2020-10-27 19:33:21 +0200

    Update interactive preview rendering for Northstar
    In RPR 2.01.7 RPR_CONTEXT_PREVIEW was implemented for Norhtstar.
    It allows us to exponentially downscale pathtracing resolution.
    RPR_CONTEXT_PREVIEW gives a more pleasant interactive preview than resolves from the render update callback.

--- pxr/imaging/plugin/hdRpr/python/generateRenderSettingFiles.py
+++ pxr/imaging/plugin/hdRpr/python/generateRenderSettingFiles.py
@@ -255,7 +255,12 @@ render_setting_categories = [
                 'defaultValue': 0.0,
                 'minValue': 0.0,
                 'maxValue': 1e6
-            },
+            }
+        ]
+    },
+    {
+        'name': 'InteractiveQuality',
+        'settings': [
             {
                 'name': 'interactiveMaxRayDepth',
                 'ui_name': 'Interactive Max Ray Depth',
@@ -263,6 +268,17 @@ render_setting_categories = [
                 'defaultValue': 2,
                 'minValue': 1,
                 'maxValue': 50
+            },
+            {
+                'name': 'interactiveResolutionDownscale',
+                'ui_name': 'Interactive Resolution Downscale',
+                'help': 'Controls how much rendering resolution is downscaled in interactive mode. Formula: resolution / (2 ^ downscale). E.g. downscale==2 will give you 4 times smaller rendering resolution.',
+                'defaultValue': 3,
+                'minValue': 0,
+                'maxValue': 10,
+                'houdini': {
+                    'hidewhen': hidewhen_not_northstar
+                }
             }
         ]
     },
