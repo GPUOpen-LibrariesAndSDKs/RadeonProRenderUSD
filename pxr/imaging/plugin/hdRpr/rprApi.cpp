3e6ee35d7ec5c128f062518d75ccc04b87d8b048
hshakula <swrqmail@gmail.com>
2020-12-02 15:58:48 +0200

    Fix crash on resizing viewport when denoising enabled

--- pxr/imaging/plugin/hdRpr/rprApi.cpp
+++ pxr/imaging/plugin/hdRpr/rprApi.cpp
@@ -1873,6 +1873,15 @@ public:
     }
 
     void UpdateAovs(HdRprRenderParam* rprRenderParam, RenderSetting<bool> enableDenoise, RenderSetting<HdRprApiColorAov::TonemapParams> tonemap, bool clearAovs) {
+        auto colorAov = GetColorAov();
+        if (colorAov) {
+            UpdateDenoising(enableDenoise, colorAov);
+
+            if (tonemap.isDirty) {
+                colorAov->SetTonemap(tonemap.value);
+            }
+        }
+
         if (m_dirtyFlags & (ChangeTracker::DirtyAOVBindings | ChangeTracker::DirtyAOVRegistry)) {
             m_resolveData.rawAovs.clear();
             m_resolveData.computedAovs.clear();
@@ -1909,15 +1918,6 @@ public:
                     outputRb.mappedData = rprRenderBuffer->GetPointerForWriting();
                     outputRb.mappedDataSize = HdDataSizeOfFormat(rprRenderBuffer->GetFormat()) * rprRenderBuffer->GetWidth() * rprRenderBuffer->GetHeight();
                 }
-            }
-        }
-
-        auto colorAov = GetColorAov();
-        if (colorAov) {
-            UpdateDenoising(enableDenoise, colorAov);
-
-            if (tonemap.isDirty) {
-                colorAov->SetTonemap(tonemap.value);
             }
         }
 
