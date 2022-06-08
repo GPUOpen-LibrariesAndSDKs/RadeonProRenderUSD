362b421ab314928546b87f2ec16c6503344a2472
Georgiy <42409851+DagerD@users.noreply.github.com>
2022-06-08 17:10:15 +0300

    BLEN-111: Render time in viewport doesn't work properly (#560)

--- pxr/imaging/plugin/hdRpr/rprApi.cpp
+++ pxr/imaging/plugin/hdRpr/rprApi.cpp
@@ -2699,11 +2699,12 @@ public:
                 }
             }
 
+            m_numSamples += m_numSamplesPerIter;
+
             auto startTime = std::chrono::high_resolution_clock::now();
 
             m_rucData.previousProgress = -1.0f;
             auto status = m_rprContext->Render();
-            m_rucData.previousProgress = -1.0f;
 
             m_frameRenderTotalTime += std::chrono::high_resolution_clock::now() - startTime;
 
@@ -2729,7 +2730,7 @@ public:
 
                     // Always force denoise on the last sample because it's quite hard to match
                     // the max amount of samples and denoise controls (min iter and iter step)
-                    if (m_numSamples + m_numSamplesPerIter == m_maxSamples) {
+                    if (m_numSamples == m_maxSamples) {
                         doDenoisedResolve = true;
                     }
                 }
@@ -2749,8 +2750,6 @@ public:
 
             // As soon as the first sample has been rendered, we enable aborting
             m_isAbortingEnabled.store(true);
-
-            m_numSamples += m_numSamplesPerIter;
         }
     }
 
