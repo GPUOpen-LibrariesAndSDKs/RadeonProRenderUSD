ac067c9bf9af0ca62d83bfb51c1930bf71e982ae
dmitrii-z <lizardman-d@yandex.ru>
2022-11-04 20:17:31 +0300

    version update

--- cmake/defaults/Version.cmake
+++ cmake/defaults/Version.cmake
@@ -23,5 +23,5 @@
 #
 # Versioning information
 set(HD_RPR_MAJOR_VERSION "2")
-set(HD_RPR_MINOR_VERSION "2")
-set(HD_RPR_PATCH_VERSION "64")
+set(HD_RPR_MINOR_VERSION "3")
+set(HD_RPR_PATCH_VERSION "0")
