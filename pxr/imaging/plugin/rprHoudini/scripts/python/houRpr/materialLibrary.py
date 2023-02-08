5d1707c823daf19a5a7e4f2120cd1cba7ad3e0d9
Viacheslav Gorash <viacheslav.gorash@gmail.com>
2023-02-09 01:04:09 +0300

    thumbnails

--- pxr/imaging/plugin/rprHoudini/scripts/python/houRpr/materialLibrary.py
+++ pxr/imaging/plugin/rprHoudini/scripts/python/houRpr/materialLibrary.py
@@ -16,9 +16,10 @@ import json
 import errno
 import shutil
 from hutil.Qt import QtCore, QtGui, QtWidgets, QtUiTools
+from PIL import Image, ImageQt
 from . materialLibraryClient import MatlibClient
 
-num = 0
+maxElementCount = 10000
 
 HELP_TEXT = '''
 To import a material click on a corresponding swatch. The material is always imported as a separate "Material Library" LOP node.
@@ -40,7 +41,7 @@ def IsMouseOnWidget(widget):
     mouse_pos_local = widget.mapFromGlobal(mouse_pos_global)
     return widget.rect().contains(mouse_pos_local)
 
-
+PREVIEW_SIZE = 200
 class LibraryListWidget(QtWidgets.QListWidget):
     def __init__(self, parent):
         super(LibraryListWidget, self).__init__(parent)
@@ -57,38 +58,60 @@ class LibraryListWidget(QtWidgets.QListWidget):
 
         #self._previewWindow = parent._fullPreviewWindow
 
-    def mouseMoveEvent(self, mouse_event):
-        if self._previewWindow.isVisible():
-            # if not IsMouseOnWidget(self):
-            #     self._previewWindow.hide()
-            # else:
-            #     self._previewWindow.mouseMoveEvent(mouse_event)
-            pass
+    # def mouseMoveEvent(self, mouse_event):
+    #     #if self._previewWindow.isVisible():
+    #         # if not IsMouseOnWidget(self):
+    #         #     self._previewWindow.hide()
+    #         # else:
+    #         #     self._previewWindow.mouseMoveEvent(mouse_event)
+    #
+    #     super(LibraryListWidget, self).mouseMoveEvent(mouse_event)
 
-        super(LibraryListWidget, self).mouseMoveEvent(mouse_event)
+    # def leaveEvent(self, event):
+    #     #if self._previewWindow.isVisible():
+    #         #if not IsMouseOnWidget(self):
+    #             # self._previewWindow.hide()
+    #             pass
+    #
+    #     super(LibraryListWidget, self).leaveEvent(event)
+    #     self._listWidget = LibraryListWidget(self)
+    #     self._ui.verticalLayout_3.insertWidget(0, self._listWidget)
 
-    def leaveEvent(self, event):
-        if self._previewWindow.isVisible():
-            if not IsMouseOnWidget(self):
-                # self._previewWindow.hide()
-                pass
+class ThumbnailLoader:
+    def __init__(self, matlib_client):
+        self._matlib_client = matlib_client
+        script_dir = os.path.realpath(os.path.dirname(__file__))
+        self._cache_dir = os.path.abspath(os.path.join(script_dir, "..", "..", "..", "..", "plugin", "usd", "rprUsd", "resources", "cache", "matlib", "thumbnails"))
+        if not os.path.isdir(self._cache_dir):
+            os.makedirs(self._cache_dir)
 
-        super(LibraryListWidget, self).leaveEvent(event)
-        self._listWidget = LibraryListWidget(self)
-        self._ui.verticalLayout_3.insertWidget(0, self._listWidget)
+    def get_thumbnail_path(self, id):
+        cached_thumbnail_path = str(os.path.join(self._cache_dir, id))
+        if(os.path.isfile(cached_thumbnail_path+"_thumbnail.jpeg")):
+            return cached_thumbnail_path+"_thumbnail.jpeg"
+        elif(os.path.isfile(cached_thumbnail_path+"_thumbnail.jpg")):
+            return cached_thumbnail_path + "_thumbnail.jpg"
+        elif (os.path.isfile(cached_thumbnail_path + "_thumbnail.png")):
+            return cached_thumbnail_path + "_thumbnail.png"
+        render_info = self._matlib_client.renders.get(id)
+        thumbnail_path = os.path.join(self._cache_dir, render_info["thumbnail"])
+        self._matlib_client.renders.download_thumbnail(id, self._cache_dir)
+        return thumbnail_path
+        
 
 class MaterialLibraryWidget(QtWidgets.QWidget):
     def __init__(self):
         super(MaterialLibraryWidget, self).__init__()
 
         self._matlib_client = MatlibClient()
+        self._thumbnail_loader = ThumbnailLoader(self._matlib_client)
         self._prev_category = None
 
         script_dir = os.path.dirname(os.path.abspath(__file__))
         ui_filepath = os.path.join(script_dir, 'materialLibrary.ui')
         self._ui = QtUiTools.QUiLoader().load(ui_filepath, parentWidget=self)
 
-        self._materialsView = QtWidgets.QListWidget(self)
+        self._materialsView = LibraryListWidget(self)
         self._ui.verticalLayout_3.insertWidget(0, self._materialsView)
 
         self._ui.helpButton.clicked.connect(self._helpButtonClicked)
@@ -101,9 +124,8 @@ class MaterialLibraryWidget(QtWidgets.QWidget):
         self._layout.addWidget(self._ui)
 
     def _initCategoryList(self):
-        category_count = self._matlib_client.categories.count()
-        categories = self._matlib_client.categories.get_list(limit=category_count)
-        for i in range(category_count):
+        categories = self._matlib_client.categories.get_list(limit=maxElementCount)
+        for i in range(len(categories)):
             self._ui.categoryView.insertItem(i, categories[i]["title"])
         self._ui.categoryView.setCurrentItem(self._ui.categoryView.item(0))
         self._ui.categoryView.clicked.connect(self._updateMaterialList)
@@ -115,12 +137,16 @@ class MaterialLibraryWidget(QtWidgets.QWidget):
             return
         self._prev_category = category
         params = {"category": category}
-        material_count = self._matlib_client.materials.count(params=params)
-        materials = self._matlib_client.materials.get_list(limit=material_count, params=params)
+        materials = self._matlib_client.materials.get_list(limit=maxElementCount, params=params)
 
         self._materialsView.clear()
-        for i in range(material_count):
-            self._materialsView.insertItem(i, materials[i]["title"])
+        for i in range(len(materials)):
+            thumbnail_id = materials[i]["renders_order"][0]
+            thumbnail_path = self._thumbnail_loader.get_thumbnail_path(thumbnail_id)
+            icon = QtGui.QIcon(QtGui.QPixmap(thumbnail_path))
+            material_item = QtWidgets.QListWidgetItem(materials[i]["title"], self._materialsView)
+            material_item.setIcon(icon)
+            self._materialsView.insertItem(i, material_item)
 
     def _helpButtonClicked(self):
         QtWidgets.QMessageBox.question(self, 'Help', HELP_TEXT, QtWidgets.QMessageBox.Ok)
@@ -146,15 +172,6 @@ class MaterialLibraryWindow(QtWidgets.QMainWindow):
         primary_screen = desktop_widget.screen(desktop_widget.primaryScreen())
         self.resize(primary_screen.width() // 3, int(primary_screen.height() * 0.8))
 
-def foo():
-    global num
-    client = MatlibClient()
-    categories = client.categories.get_list(client.categories.count())
-    params = {"category": categories[num]["title"]}
-    materials = client.materials.get_list(limit=client.materials.count(params=params), params=params)
-    num += 1
-    for material in materials:
-        print(material["title"])
 
 def import_material():
     window = MaterialLibraryWindow()
