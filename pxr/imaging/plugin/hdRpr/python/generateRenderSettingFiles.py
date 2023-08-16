a1a273ac67d8e8ccc9f0dc8643c8132df8556021
dmitrii-z <43916437+dmitrii-z@users.noreply.github.com>
2023-08-16 06:22:09 +0300

    sampling defaults update (#686)
    * sampling defaults update
    
    * another update

--- pxr/imaging/plugin/hdRpr/python/generateRenderSettingFiles.py
+++ pxr/imaging/plugin/hdRpr/python/generateRenderSettingFiles.py
@@ -308,7 +308,7 @@ render_setting_categories = [
                 'name': 'maxSamples',
                 'ui_name': 'Max Samples',
                 'help': 'Maximum number of samples to render for each pixel.',
-                'defaultValue': 256,
+                'defaultValue': 128,
                 'minValue': 1,
                 'maxValue': 2 ** 16
             }
@@ -324,7 +324,7 @@ render_setting_categories = [
                 'name': 'adaptiveSampling:minSamples',
                 'ui_name': 'Min Samples',
                 'help': 'Minimum number of samples to render for each pixel. After this, adaptive sampling will stop sampling pixels where noise is less than \'Variance Threshold\'.',
-                'defaultValue': 64,
+                'defaultValue': 32,
                 'minValue': 1,
                 'maxValue': 2 ** 16
             },
@@ -332,7 +332,7 @@ render_setting_categories = [
                 'name': 'adaptiveSampling:noiseTreshold',
                 'ui_name': 'Noise Threshold',
                 'help': 'Cutoff for adaptive sampling. Once pixels are below this amount of noise, no more samples are added. Set to 0 for no cutoff.',
-                'defaultValue': 0.0,
+                'defaultValue': 0.05,
                 'minValue': 0.0,
                 'maxValue': 1.0
             }
