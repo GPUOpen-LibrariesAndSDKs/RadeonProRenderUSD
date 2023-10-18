3000adc110ac34cf366bb094a7982977bcc74c7d
Vyacheslav Gorash <viacheslav.gorash@gmail.com>
2023-10-18 20:27:48 +0300

    Fix output adding (#706)

--- pxr/imaging/rprUsd/hdMtlxFixed.cpp
+++ pxr/imaging/rprUsd/hdMtlxFixed.cpp
@@ -316,15 +316,15 @@ _GatherUpstreamNodes(
             mx::NodePtr mxNextNode = *mxUpstreamNode;
 
             mx::OutputPtr mxOutput = mxNextNode->getOutput(currConnection.upstreamOutputName);
-            if (!mxOutput) {
-              mxOutput = mxNextNode->addOutput(currConnection.upstreamOutputName);
+            if (!mxOutput && mxNextNode->getType() == "multioutput") {
+                mxOutput = mxNextNode->addOutput(currConnection.upstreamOutputName);
             }
 
             // Make sure to not add the same input twice
             mx::InputPtr mxInput = mxCurrNode->getInput(connName);
             if (!mxInput){
-              mxInput = mxCurrNode->addInput(connName, mxNextNode->getType() == "multioutput" ?
-                mxOutput->getType() : mxNextNode->getType());
+                mxInput = mxCurrNode->addInput(connName, mxNextNode->getType() == "multioutput" ?
+                  mxOutput->getType() : mxNextNode->getType());
             }
             mxInput->setConnectedNode(mxNextNode);
         }
