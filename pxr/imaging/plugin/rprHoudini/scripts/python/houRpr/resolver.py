8b9cabe1076b8cad176d4c459d3ae6e5d748f944
VGorash <gorash590@gmail.com>
2023-10-24 02:24:40 +0300

    ui improvements

--- pxr/imaging/plugin/rprHoudini/scripts/python/houRpr/resolver.py
+++ pxr/imaging/plugin/rprHoudini/scripts/python/houRpr/resolver.py
@@ -2,6 +2,7 @@ from pxr import Plug
 from time import sleep
 
 import hou
+import os
 
 usd_plugin = Plug.Registry().GetPluginWithName('RenderStudioResolver')
 if not usd_plugin.isLoaded:
@@ -21,10 +22,19 @@ def get_shared_workspace_menu():
 def toggle_shared_workspace(command):
     global shared_workspace_enabled
 
-
     if command == "enable":
+        code, server_url = hou.ui.readInput("Storage server URL", buttons=('OK',),
+                                      severity=hou.severityType.Message, help=None, title=None,
+                                      initial_contents=RenderStudioKit.GetWorkspaceUrl())
+        if code == -1 or server_url == "":
+            return
+        if not server_url.startswith("http"):
+            hou.ui.displayMessage("The URL should start from 'http://' or 'https://'", severity=hou.severityType.Warning)
+            toggle_shared_workspace("enable")
+            return
         try:
             with hou.InterruptableOperation("Connecting to shared workspace", open_interrupt_dialog=True) as op:
+                RenderStudioKit.SetWorkspaceUrl(server_url)
                 RenderStudioKit.SharedWorkspaceConnect()
                 shared_workspace_enabled = True
         except hou.OperationInterrupted:
@@ -42,3 +52,15 @@ def toggle_shared_workspace(command):
         except:
             raise Exception('Error on shared workspace disconnecting')
 
+
+def set_workspace_directory():
+    directory = hou.ui.selectFile(
+        title='Render Studio Workspace Directory',
+        start_directory=RenderStudioKit.GetWorkspacePath().replace('\\', '/'),
+        file_type=hou.fileType.Directory,
+        chooser_mode=hou.fileChooserMode.Write)
+    if directory:
+        RenderStudioKit.SetWorkspacePath(hou.expandString(directory))
+
+def open_workspace_directory():
+    os.system("explorer " + RenderStudioKit.GetWorkspacePath())
